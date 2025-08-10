c31d24761d5bec3ff8c7e83f1db43a0b23386969
Torrenté Florian <lukyrouge4@gmail.com>
2025-08-10 19:37:43 +0200

    feat: Add 3D object management with socket.io and THREE.js in index.vue chore: Update eslint configuration and add types for three.js

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,142 @@
+<script setup lang="ts">
+	import { io } from 'socket.io-client';
+	import * as THREE from 'three'
+
+	const ObjectTypes = {
+	  Cube: 'CUBE',
+	  Sphere: 'SPHERE',
+	  Pyramid: 'PYRAMID'
+	} as const;
+
+	type SceneObject = {
+		objectType: {label: string, value: typeof ObjectTypes[keyof typeof ObjectTypes] | undefined},
+		size: number,
+		position: {x: number, y: number, z: number},
+		color: string
+	};
+
+	const typeOptions = [
+		{ label: 'Cube', value: ObjectTypes.Cube },
+		{ label: 'Sphere', value: ObjectTypes.Sphere },
+		{ label: 'Pyramid', value: ObjectTypes.Pyramid }
+	]
+
+	const formState = reactive<SceneObject>({
+		objectType: { label: 'Cube', value: ObjectTypes.Cube },
+		size: 1,
+		position: { x: 0, y: 0, z: 0 },
+		color: '#ff0000'
+	});
+
+	const socket = io('http://localhost:5000'); // TODO: Get url from env
+
+	socket.emit('scene_data', null, (sceneData: {objects: SceneObject[]}) => {
+		sceneObjects.value = sceneData.objects;
+		renderScene();
+	});
+
+	socket.on('object_added', (newObject: SceneObject) => {
+		sceneObjects.value.push(newObject);
+		renderScene();
+	});
+
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+	const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+	const sceneObjects = ref<SceneObject[]>([]);
+	let objects: THREE.Mesh[] = [];
+
+	function addObject() {
+		socket.emit("add_object", formState);
+	}
+
+	function initLights() {
+		const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+		scene.add(ambient);
+		const directional = new THREE.DirectionalLight(0xffffff, 0.7);
+		directional.position.set(5, 10, 7.5);
+		scene.add(directional);
+	}
+
+	function initThree() {
+		const container = document.getElementById('three')!
+		const { clientWidth, clientHeight } = container
+		renderer.setSize(clientWidth, clientHeight)
+		renderer.setPixelRatio(window.devicePixelRatio)
+		container.appendChild(renderer.domElement)
+
+		camera.aspect = clientWidth / clientHeight
+		camera.updateProjectionMatrix()
+		camera.position.set(0, 2, 5)
+
+		initLights()
+
+		// Lancer la boucle de rendu
+		const animate = () => {
+			requestAnimationFrame(animate)
+			renderer.render(scene, camera)
+		}
+		animate()
+	}
+
+	function clearScene() {
+		objects.forEach((obj) => {
+			scene.remove(obj);
+			obj.geometry.dispose();
+
+			// Clear material too
+			if ((obj.material as any).dispose) {
+				(obj.material as any).dispose();
+			}
+		})
+		objects = [];
+	}
+
+	function renderScene() {
+		clearScene();
+		objects = sceneObjects.value.map((obj) => {
+			const geometry = new THREE.BoxGeometry(obj.size, obj.size, obj.size);
+			const material = new THREE.MeshStandardMaterial({ color: obj.color });
+			const mesh = new THREE.Mesh(geometry, material);
+			mesh.position.set(obj.position.x, obj.position.y, obj.position.z);
+			scene.add(mesh);
+			return mesh;
+		});
+	}
+
+	onMounted(() => {
+		initThree();
+	});
+
+</script>
+
+<template>
+  <div class="p-4 flex flex-col">
+		<div id="three" class="absolute inset-0"/>
+
+		<UForm :state="formState" class="max-w-xl z-10 border border-white rounded-lg p-4" @submit.prevent="addObject">
+			<UFormField label="Select Object Type" name="objectType">
+				<USelect v-model="formState.objectType.value" :items="typeOptions" />
+			</UFormField>
+
+			<UFormField label="Size" name="size">
+				<UInputNumber v-model="formState.size" type="number" :step="0.1" />
+			</UFormField>
+
+			<UFormField label="Position" name="position" class="">
+				<UInputNumber v-model="formState.position.x" type="number" :step="0.1" placeholder="X" />
+				<UInputNumber v-model="formState.position.y" type="number" :step="0.1" placeholder="Y" />
+				<UInputNumber v-model="formState.position.z" type="number" :step="0.1" placeholder="Z" />
+			</UFormField>
+
+			<UFormField label="Color" name="color">
+				<UColorPicker v-model="formState.color" />
+			</UFormField>
+
+			<div class="mt-4">
+        <UButton type="submit">Ajouter l'objet</UButton>
+      </div>
+		</UForm>
+	</div>
+</template>
--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,4 +3,14 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module'
+      }
+    }
+  }
 )
